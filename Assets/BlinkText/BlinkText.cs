30f4c0df1d6634f660362661b87eb68d930fb442
Makoto Shinoda <metal.shinoda@gmail.com>
2017-03-20 17:00:46 +0900

    Init Commit

---
+++ Assets/BlinkText/BlinkText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class BlinkText : MonoBehaviour
+{
+    bool canBlink = true;   //点滅できるか.
+    Animator animator;       //アニメーターへの参照.
+
+    /// <summary>
+    /// 点滅アニメーション終了時(Animation Event)に呼び出す.
+    /// </summary>
+    void EndBlink()
+    {
+        canBlink = true;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        //アニメーターコンポーネントを取得する.
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //何かキーが押されたとき.
+        if (Input.anyKeyDown)
+        {
+            if (canBlink)
+            {
+                animator.SetTrigger("TriggerBlink");
+                canBlink = false;
+            }
+        }
+    }
+}
